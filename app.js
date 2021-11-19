b6fb53ba21f7f1621e2644d908e3da43f66e98a8
Danny Cairns <dpcairns@gmail.com>
2021-11-18 16:30:50 -0800

    initial commit

--- app.js
+++ app.js
@@ -1,8 +1,15 @@
-// import functions and grab DOM elements
+import { getDogs } from './fetch-utils.js';
+import { renderDogCard } from './render-utils.js';
 
-// initialize state
+const dogListContainer = document.getElementById('dog-list-container');
 
-// set event listeners 
-  // get user input
-  // use user input to update state 
-  // update DOM to reflect the new state
+window.addEventListener('load', async() => {
+    // fetch all dogs
+    const dogs = await getDogs();
+
+    for (let dog of dogs) {
+        const dogEl = renderDogCard(dog);
+      // render the dog detail
+        dogListContainer.append(dogEl);
+    }
+});
